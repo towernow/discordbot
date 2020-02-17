985a9874b509ccda9fb5ddb4a7cd326213543aff
Sergi Torrella <torryxd@gmail.com>
2020-02-17 17:34:49 +0100

    c

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const bot = new Discord.Client();
 const token = 'Njc2MTI0Mzk1Nzg1NDIwODAx.Xkqy5Q.jpuZuQRI1Lw0eoX0z17uc10UMws';
 
 bot.on('ready', () => {
-	console.log('BOT IS UP!');
+	console.log('BOT IS ONLINE!');
 })
 
 bot.on('message', msg => {
 	if (msg.content === "hola" || msg.content === "Hola") {
-		msg.reply('hola que tal bro');
+		msg.reply('hola que tal loko');
 	}else if (msg.content === "HOLA") {
 		msg.reply('no grites loko');
 	}
