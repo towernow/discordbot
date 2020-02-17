72adbf68630b7b437123cf880b4246452d308673
Sergi Torrella <torryxd@gmail.com>
2020-02-17 17:54:32 +0100

    update

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const pre = '-';
 const token = 'Njc2MTI0Mzk1Nzg1NDIwODAx.Xkqy5Q.jpuZuQRI1Lw0eoX0z17uc10UMws';
-var dc = "";
 
 bot.on('ready', () => {
 	console.log('BOT IS ONLINE!');
 })
 
 bot.on('message', msg => {
+	var dc = msg.channel.id;
+
 	if (msg.content === "hola" || msg.content === "Hola") {
 		dc.send('que tal bro');
 	}else if (msg.content === "HOLA") {
 		dc.send('no grites loko');
-	}
-
-	if (msg.content === pre + "setdefaultchannel") {
-		dc = msg.channel_id;
 	}
 })
 
